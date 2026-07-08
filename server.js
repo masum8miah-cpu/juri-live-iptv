85779897ceed09ccd21e8326222eab4eef10217f
masum8miah-cpu <masum8miah@gmail.com>
2026-07-09 00:53:03 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// আপনার GitHub Pages M3U
+const PLAYLIST =
+  "https://masum8miah-cpu.github.io/my-ip-tv/%E0%A6%AC%E0%A7%8D%E0%A6%B0%E0%A6%BE%E0%A6%87%E0%A6%9F%E0%A6%BF%E0%A6%9C.m3u";
+
+// আপনার টোকেন
+const TOKEN = "billal2026";
+
+app.get("/playlist.m3u", async (req, res) => {
+  if (req.query.token !== TOKEN) {
+    return res.status(403).send("Access Denied");
+  }
+
+  try {
+    const response = await fetch(PLAYLIST);
+    const text = await response.text();
+
+    res.setHeader("Content-Type", "application/x-mpegURL");
+    res.send(text);
+  } catch (err) {
+    res.status(500).send("Failed to load playlist");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
